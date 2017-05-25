37e50903fbaccaaba1d030ba9af9a65a36264180
lucassodre <lucassodrebam8@gmail.com>
2017-05-25 17:02:04 -0300

    src/com/pbj/loccar/persistence/PersistenceLocacao.java: criei a classe PersistenceLocacao na persistencia

---
+++ src/com/pbj/loccar/persistence/PersistenceLocacao.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pbj.loccar.persistence;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ *
+ * @author lucas
+ */
+public class PersistenceLocacao {
+    private static Statement stmt;
+    private static ResultSet rs;
+    
+    private static Conexao conexao = new Conexao();
+}
