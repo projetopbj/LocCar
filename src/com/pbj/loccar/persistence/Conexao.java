994c9b02bc01422c832f274747f60bd6861a1a12
lucassodre <lucassodrebam8@gmail.com>
2017-05-25 09:02:20 -0300

    src/com/pbj/loccar/persistence/: criei a percistencia

---
+++ src/com/pbj/loccar/persistence/Conexao.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pbj.loccar.persistence;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author lucas
+ */
+public class Conexao {
+    private static Connection con;
+    private static String url;
+    private static String username;
+    private static String password;
+    
+//    passar o url,username e password como sendo root,...
+    public Connection getConnection(){
+        
+        if(con == null){
+            try {
+                con = DriverManager.getConnection(url, username, password);
+            } catch (Exception e) {
+                System.out.println("Erro ao conectar com o banco " + e);
+            }
+        }
+        return con;
+    }
+    
+    
+}
