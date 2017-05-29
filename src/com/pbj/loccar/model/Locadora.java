9b2ab09d4bb55fec7638bf6586d4bdfe3d8722ad
lucassodre <lucassodrebam8@gmail.com>
2017-05-29 15:33:51 -0300

    src/com/pbj/loccar/model/: criai o modelo e as classes

---
+++ src/com/pbj/loccar/model/Locadora.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pbj.loccar.model;
+
+/**
+ *
+ * @author lucas
+ */
+public class Locadora {
+    
+    public static void Cadastro(){
+    
+    }
+    
+    public static void Listar(){
+    
+    }
+    
+    public static void Alterar(){
+    
+    }
+}
