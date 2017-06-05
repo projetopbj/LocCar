4022594cb7d697f6da947e586ec742be03e1194d
lucassodre <lucassodrebam8@gmail.com>
2017-06-05 16:27:02 -0300

    criei o teste para categoria

---
+++ src/com/pbj/loccar/teste/testeCategoriaDAO.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pbj.loccar.teste;
+
+import com.pbj.loccar.model.Locadora;
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author lucas
+ */
+public class testeCategoriaDAO extends TestCase{
+    
+}
