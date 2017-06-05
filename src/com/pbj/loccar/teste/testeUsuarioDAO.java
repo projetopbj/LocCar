e6cf07b804537a63ef98500e6b7dceb39785c10a
lucassodre <lucassodrebam8@gmail.com>
2017-06-05 16:27:34 -0300

    criei a classe de teste para usuário

---
+++ src/com/pbj/loccar/teste/testeUsuarioDAO.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pbj.loccar.teste;
+
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author lucas
+ */
+public class testeUsuarioDAO extends TestCase{
+    
+}
