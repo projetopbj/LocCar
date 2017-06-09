c934faeca12e4885cf0cd9c1b012a35d3325bc50
lucassodre <lucassodrebam8@gmail.com>
2017-06-09 11:39:30 -0300

    test/com/pbj/loccar/Model/UsuarioDaoTest.java: adicionando novas funções a classe de teste

--- test/com/pbj/loccar/Model/UsuarioDaoTest.java
+++ test/com/pbj/loccar/Model/UsuarioDaoTest.java
@@ -30,22 +30,64 @@ public class UsuarioDaoTest extends TestCase{
     }
     
     public void testcreateUsuario(){
+        usuario.setAcessoUser("Admin");
+        usuario.setId(1);
+        usuario.setLoginUser("Admin");
+        usuario.setNomeUser("Admin");
+        usuario.setSenhaUser("Admin");
+        
         usuarioDAO.createUsuario(usuario);
+        
+        assertEquals(0,0);
     }
     
     public void testreadUsuario(){
+        usuario.setAcessoUser("Admin");
+        usuario.setId(1);
+        usuario.setLoginUser("Admin");
+        usuario.setNomeUser("Admin");
+        usuario.setSenhaUser("Admin");
+        
         usuarioDAO.readUsuario();
+        
+        assertEquals(0,0);
     }
     
     public void testloginUsuario(){
+        usuario.setAcessoUser("Admin");
+        usuario.setId(1);
+        usuario.setLoginUser("Admin");
+        usuario.setNomeUser("Admin");
+        usuario.setSenhaUser("Admin");
+        
         usuarioDAO.loginUsuario("Admin", "Admin");
+        
+        assertEquals(0,0);
     }
     
     public void testupdateUsuario(){
+        
+        usuario.setAcessoUser("Admin");
+        usuario.setId(1);
+        usuario.setLoginUser("Admin");
+        usuario.setNomeUser("Admin");
+        usuario.setSenhaUser("Admin");
+        
         usuarioDAO.updateUsuario(usuario);
+        
+        assertEquals(0,0);
     }
     
     public void testdeleteUsuario(){
+        
+        usuario.setAcessoUser("Admin");
+        usuario.setId(1);
+        usuario.setLoginUser("Admin");
+        usuario.setNomeUser("Admin");
+        usuario.setSenhaUser("Admin");
+        
         usuarioDAO.deleteUsuario(usuario);
+        
+        assertEquals(0,0);
     }
 }
