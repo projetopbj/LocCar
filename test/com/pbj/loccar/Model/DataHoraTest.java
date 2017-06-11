329fb0a3f2eaab4df7bc615ee4f16217c2b81a68
lucassodre <lucassodrebam8@gmail.com>
2017-06-11 10:40:09 -0300

    criando a classe de teste de datahora

---
+++ test/com/pbj/loccar/Model/DataHoraTest.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pbj.loccar.Model;
+
+import com.pbj.loccar.util.DataHora;
+
+/**
+ *
+ * @author lucas
+ */
+public class DataHoraTest {
+    
+    // declaro as variaveis usadas
+    DataHora datahora;
+    
+    // inicio as variaveis
+    public void setUp(){
+        datahora = new DataHora();
+        
+    }
+
+    // fecho as variaveis
+    public void tearDown(){
+        datahora = null;
+    }
+
+    // testo o metodo
+    public void getHoraTest(){
+
+    }
+
+    // testo o metodo
+    public void getDataTest(){
+        
+    }
+
+    // testo o metodo
+    public void somaDiasTest(){
+    
+    }
+
+    // testo o metodo
+    public void dataToStringTest(){
+    
+    }
+
+    // testo o metodo
+    public void stringToDataTest(){
+    
+    }
+    
+    // testo o metodo
+    public void convertDataTest(){
+    
+    }
+    
+}
